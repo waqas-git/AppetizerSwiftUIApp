1f38b103a1b4f32a96feb0e6a77d7d9e53824b16
Waqas Ahmed <waqasahmed.uol@gmail.com>
2024-08-14 23:16:29 +0500

    Initial Commit

---
+++ AppitizerSwiftUIApp/AppitizerSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppitizerSwiftUIAppApp.swift
+//  AppitizerSwiftUIApp
+//
+//  Created by waqas ahmed on 14/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppitizerSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
